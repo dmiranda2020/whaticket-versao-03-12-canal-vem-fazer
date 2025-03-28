e3717e50989c18ff9ce8e7174c7aa4baf6e0ad7e
dmiranda2020 <diegomiranda-@hotmail.com>
2025-03-27 21:29:04 -0300

    Update 20200904070005-create-default-company.ts

--- backend/src/database/seeds/20200904070005-create-default-company.ts
+++ backend/src/database/seeds/20200904070005-create-default-company.ts
@@ -8,7 +8,7 @@ module.exports = {
           "Plans",
           [
             {
-              name: "Plano 1",
+              name: "Plano1",
               users: 10,
               connections: 10,
               queues: 10,
@@ -23,7 +23,7 @@ module.exports = {
           "Companies",
           [
             {
-              name: "Empresa 1",
+              name: "Empresa1",
               planId: 1,
               dueDate: "2093-03-14 04:00:00+01",
               createdAt: new Date(),
